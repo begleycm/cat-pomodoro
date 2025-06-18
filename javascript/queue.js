8616ed1443c0a8d8829b1de3fe58e9054f02d8b5
Connor <cbegley027@gmail.com>
2025-06-17 23:44:21 -0400

    Revert "add queueClass and part of the queue logic"
    This reverts commit 886e5786952885f2815cffa3e66722be81ceb169.

--- javascript/queue.js
+++ javascript/queue.js
@@ -1,7 +1,4 @@
-// TO COMPILE THE TYPESCRIPT FILES, RUN "npm run build" ON THE COMMAND LINE
 import { QueueObject } from './queueObject.js';
-import { QueueClass } from './QueueClass.js';
-const queue = new QueueClass(100); // len limit is 100 for now
 // Create a namespace for our queue functions
 const QueueFunctions = {
     addStudyToQueue: function () {
@@ -11,7 +8,6 @@ const QueueFunctions = {
             time: 25,
         });
         queue_container.appendChild(newQueueObject.getElement());
-        queue.enQueue(newQueueObject); // enqueue new study
     },
     addBreakToQueue: function () {
         console.log("add break called");
@@ -20,7 +16,6 @@ const QueueFunctions = {
             time: 0,
         });
         queue_container.appendChild(newQueueObject.getElement());
-        queue.enQueue(newQueueObject); // enqueue new break
     }
 };
 // Add to window object
@@ -30,18 +25,4 @@ if (!queue_container) {
     throw new Error("Could not find .queue_objects_container in the DOM");
 }
 console.log("creating queue obj");
-//////////////////////////////////////////////////////// Queue Logic ////////////////////////////////////////////////////////////////////////////////////
-// Thinking about queue logic
-// If the queue becomes empty, load the opposite of what was last loaded.
-// Otherwise, go to the next item in the queue.
-// Im using a queue (who would've thought) data structure to represent these things.
-// The user is allowed to add like 20 breaks in a row if they desire.
-if (queue.isEmpty()) {
-    // proceed normally
-}
-else {
-    // dequeue last queue object and use it as the current time
-    const nextQueue = queue.deQueue();
-    // logic here that sets the timer to that time
-}
 //# sourceMappingURL=queue.js.map
